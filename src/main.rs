7f6eaadd7fb4b9136495af09694d813ed3923eab
Albert Goncalves <goncalves.albert@gmail.com>
2020-08-08 11:53:29 -0400

    parse file via stdin

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,12 @@
 mod parser;
 mod tokenizer;
+
+use crate::parser::get_ast;
+use crate::tokenizer::get_tokens;
+use std::{io, io::Read};
+
+fn main() {
+    let mut buffer = String::new();
+    let _: usize = io::stdin().read_to_string(&mut buffer).unwrap();
+    println!("{:#?}", get_ast(&get_tokens(&buffer)));
+}
