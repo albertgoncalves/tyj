0532b0637f19868d4d4bf2735c8d0745581e9485
Albert Goncalves <goncalves.albert@gmail.com>
2020-08-08 16:16:01 -0400

    remove unwrap, still same deal

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ mod tokenizer;
 
 use crate::parser::get_ast;
 use crate::tokenizer::get_tokens;
-use std::{io, io::Read};
+use std::io::{stdin, Read};
 
 fn main() {
     let mut buffer = String::new();
-    let _: usize = io::stdin().read_to_string(&mut buffer).unwrap();
+    let _: Result<usize, _> = stdin().read_to_string(&mut buffer);
     println!("{:#?}", get_ast(&get_tokens(&buffer)));
 }
