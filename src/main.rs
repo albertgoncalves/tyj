3db9297bc07ad2b40854c62f4eecdacb783bedf8
Albert Goncalves <goncalves.albert@gmail.com>
2020-08-20 23:43:10 -0400

    basic parse error message

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
 mod parser;
 mod tokenizer;
-mod typer;
+// mod typer;
 
-use crate::parser::get_ast;
-use crate::tokenizer::get_tokens;
-use crate::typer::get_types;
-use std::io::{stdin, Read};
+use crate::parser::{get_ast, Error};
+use crate::tokenizer::{get_tokens, Count};
+// use crate::typer::get_types;
+use std::env::args;
+use std::fs::read_to_string;
+use std::process::exit;
+
+macro_rules! PARSE_ERROR {
+    () => {
+        "\x1b[1m{}\x1b[0m:\
+         \x1b[1;4m{}\x1b[0m:\
+         \x1b[1;31mParse Error\x1b[0m"
+    };
+}
+
+macro_rules! EXIT {
+    () => {
+        exit(1)
+    }
+}
 
 fn main() {
-    let mut buffer = String::new();
-    let _: Result<usize, _> = stdin().read_to_string(&mut buffer);
-    println!("{:#?}", get_types(&get_ast(&get_tokens(&buffer))));
+    let args: Vec<String> = args().collect();
+    if args.len() < 2 {
+        EXIT!();
+    }
+    let filename: &str = &args[1];
+    let source: String = match read_to_string(filename) {
+        Ok(source) => source,
+        _ => EXIT!(),
+    };
+    match get_ast(&get_tokens(&source)) {
+        Ok(ast) => println!("{:#?}", ast),
+        Err(error) => {
+            let line: Count = match error {
+                Error::EOF => {
+                    let mut line: Count = 0;
+                    for x in source.chars() {
+                        if x == '\n' {
+                            line += 1;
+                        }
+                    }
+                    line
+                }
+                Error::Token(token) => token.line + 1,
+            };
+            eprintln!(PARSE_ERROR!(), filename, line);
+            EXIT!()
+        }
+    }
 }
