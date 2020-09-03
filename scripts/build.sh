fd6a4d2f471f714a47f89cd20d98b7e75fa8a0ec
albertgoncalves <goncalves.albert@gmail.com>
2020-09-02 21:37:46 -0400

    incremental builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,16 @@ set -eux
 . "$WD/scripts/flags.sh"
 
 if [ "$1" = "test" ]; then
-    rustc -o "$WD/bin/test" "${FLAGS[@]}" --test "$WD/src/main.rs"
+    rustc \
+        -C "incremental=$WD/cache/test" \
+        -o "$WD/bin/test" \
+        "${FLAGS[@]}" \
+        --test \
+        "$WD/src/main.rs"
 elif [ "$1" = "main" ]; then
-    rustc -o "$WD/bin/main" "${FLAGS[@]}" "$WD/src/main.rs"
+    rustc \
+        -C "incremental=$WD/cache/main" \
+        -o "$WD/bin/main" \
+        "${FLAGS[@]}" \
+        "$WD/src/main.rs"
 fi
